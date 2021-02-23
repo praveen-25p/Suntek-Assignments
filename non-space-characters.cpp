4e8bdef68f642d8e184b0489ac532ea2891d93f0
Praveen Kumar Sahu <praveen257000@gmail.com>
2021-02-24 01:42:29 +0530

    #Assignment 3 non space characters

---
+++ non-space-characters.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int countSegments(string s) {
+        int count=0;
+        for(int i=0 ;i<s.length();i++)
+        {
+        if(s[i]!=' ')
+        {
+            while(s[i]!=' ' && i<s.length())
+            {
+                i++;    
+            }
+            count++;        
+        }
+        }
+        return count;
+    }
+};
+int main()
+{
+    string a="this is a training program";
+    Solution p;
+    cout<<(p.countSegments(a));
+}
